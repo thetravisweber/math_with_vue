221bd44e4fe419184eb62277cdb631f79e18605a
thetravisweber <tbroweber@gmail.com>
2021-11-19 17:05:15 -0700

    color, linear sizing, gold primes

--- src/components/PrimeFactorsTable.vue
+++ src/components/PrimeFactorsTable.vue
@@ -5,15 +5,16 @@
     <table id="prime-factor-table">
       <tr>
         <th></th>
-        <th class="titler" v-for="(prime, i) in primes" :key="i" :class="headerClass">
+        <th class="titler" v-for="(prime, i) in primes" :key="i" :class="headerClass" :style="columnStyle(prime)">
           {{ prime }}
         </th>
       </tr>
       <tr
         v-for="(factors, factored_num) in factoredTableData"
         :key="factored_num"
+        :style="rowStyle(factored_num)"
       >
-        <td :class="headerClass">{{ factored_num + 2 }}</td>
+        <td :class="headerClass" :style="goldIfPrime(factored_num+2)">{{ factored_num + 2 }}</td>
         <td v-for="(num_of_factor, j) in factors" :key="j" :style="factorStyle(num_of_factor)">
           {{ display_number_of_factors(num_of_factor, abstract_symbol) }}
         </td>
@@ -33,7 +34,7 @@ export default {
   data: function () {
     return {
       primes: [2, 3],
-      max_number: 40
+      max_number: 69
     };
   },
   computed: {
@@ -71,21 +72,53 @@ export default {
       this.primes.push(num);
       return this.factor(num);
     },
-    factorStyle: (num_of_factor) => {
-      let hue = 200 + 5 * num_of_factor; // this.map(num_of_factor, 0, 3, 140, 220);
-      let rgb = this.HSBToRGB(hue, 100, 100);
-      console.log(`rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`);
-      return {
-        color: "#FFFFFF"
-      };
-    },
-    HSBToRGB: (h, s, b) => {
+
+    HSBToRGB: function(h, s, b) {
       s /= 100;
       b /= 100;
       const k = (n) => (n + h / 60) % 6;
       const f = (n) => b * (1 - s * Math.max(0, Math.min(k(n), 4 - k(n), 1)));
       return [255 * f(5), 255 * f(3), 255 * f(1)];
     },
+
+    sizeMap: function (number) {
+      return this.map(number, 0, this.max_number, 25, 0);
+    },
+
+    columnStyle: function(prime) {
+      let size = this.map(prime, 0, this.max_number, 25, 10);
+      return {
+        "font-size": size + "px",
+        "width": (size*1.1)+"px"
+      };
+    },
+
+    rowStyle: function(row_number) {
+      let size = this.sizeMap(row_number);
+      return {
+        "font-size": size + "px",
+        "height": (size*1.1)+"px"
+      };
+    },
+
+    factorStyle: function(num_of_factor) {
+      let hue = this.map(num_of_factor, 0, 3, 140, 220);
+      let rgb = this.HSBToRGB(hue, 100, 100);
+      return {
+        color: `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
+      };
+    },
+
+    goldIfPrime: function(number) {
+      if (this.primes.includes(number)) {
+        console.log("got a prime");
+        return {
+          color: "rgb(204,164,61)"
+        };
+      }
+      return {};
+    },
+
     map: function (num, start1, end1, start2, end2) {
       num -= start1;
       num /= (end1-start1);
@@ -93,6 +126,7 @@ export default {
       num += start2;
       return num;
     },
+
     display_number_of_factors: (num, abstract_symbol) => {
       if (num == 0) {
         return '';
@@ -122,6 +156,7 @@ export default {
 .prime-factors {
   background-color: black;
   font-size: 25px;
+  margin: 0px;
 }
 .prime-factor-table {
   background: -webkit-linear-gradient(#eee, #333);
@@ -131,17 +166,13 @@ export default {
 }
 
 .white-header {
-  -webkit-text-fill-color: white;
+  color: white;
 }
 
 td {
   width: 32px;
   text-align: center;
+  margin: 0px;
 }
-
-@function hsb($h-hsb, $s-hsb, $b-hsb, $a: 1)
-$l-hsl: ($b-hsb/2) * (2 - ($s-hsb/100));
-$s-hsl: ($b-hsb * $s-hsb) / if($l-hsl < 50, $l-hsl * 2, 200 - $l-hsl * 2);
-@return hsla($h-hsb, $s-hsl, $l-hsl, $a);
 
 </style>
